f9a9902070d3d81c7a749ce5230e8878c3e0a7c1
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-25 08:27:49 +0500

    Url added of the web to be scrapped

---
+++ index.js
@@ -0,0 +1,6 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+// url for the web to be scraped
+const url = 'https://sfbay.craiglist.org/d/software-qa-dba-etc/search/sof' 
+
