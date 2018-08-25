0f60a2b3f7094070cd96e6ece5a99e134f721003
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-25 08:32:24 +0500

    try catch added aling with url typo fixed

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const request = require('request-promise');
 const cheerio = require('cheerio');
 
 // url for the web to be scraped
-const url = 'https://sfbay.craiglist.org/d/software-qa-dba-etc/search/sof' 
+const url = 'https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof' 
 
-async function scrapeCraglist() {
+async function scrapeCragslist() {
     try{
-        const htmlRequest = await request.get(url)
-        console.log(htmlRequest)
+        const htmlRequest = await request.get(url);
+        console.log(htmlRequest);
     }catch(err){
 
-    }
-
-    
+    }   
 }
+
+scrapeCraglist()
