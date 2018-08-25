d2e726f6157ad68624f662c606e703cb320422ee
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-25 08:28:58 +0500

    get Request added

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const cheerio = require('cheerio');
 // url for the web to be scraped
 const url = 'https://sfbay.craiglist.org/d/software-qa-dba-etc/search/sof' 
 
+async function scrapeCraglist() {
+    
+    const htmlRequest = await request.get(url)
+
+    
+}
