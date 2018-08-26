9b683dfaca726afd33bd99c4d65e096ff3b55870
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-08-26 11:29:07 +0500

    comments added to the file

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const request = require('requestretry');
+// using requestretry for low connectivity
+// using cheerio for loading html tags
+// objects-to-csv
+const request = require('requestretry').defaults({fullResponse: false});
 const cheerio = require('cheerio');
 const ObjectsToCsv = require('objects-to-csv');
 
 // url for the web to be scraped
 const url = 'https://sfbay.craigslist.org/d/software-qa-dba-etc/search/sof' 
-const scrapeResult = {
-    titl: "Technical Autonomous Vehicle Trainer",
-    description: "We're the driverless car company we're buidling.....",
-    datePosted:new Date(2018-07-13),
-    url: "https://sfbay.craigslist.org/sfc/sof/d/technical-autonomous-vehicle/6642626746.html",
-    hood: '(SOMA / South Beach)',
-    address: "1201 Brant St.",
-    compensation: '23/hr'
-}
-
 
 // see if the jqeusry is enabled on the page by typing $ on the console.
 
@@ -22,27 +15,39 @@ const scrapeResult = {
     console..log($(element).children(id/class).text())
 })*/
 
+// final results of scrape
 const scrapeResults = []
 async function scrapeJobHeader() {
     try{
+        // gets the html tags of the given url
         const htmlRequest = await request.get(url);
-        // console.log(htmlRequest);
-  
+        
+        
+        // loads the tags of url into querying
         const $ = await cheerio.load(htmlRequest);
         
+        // targets the parent node using class selector
         $(".result-info").each((index,element) => {
             
+            //  gets the title of the job
             const resultTitle = $(element).children(".result-title");
             const title = resultTitle.text();
+
+            // gets the url of the job
             const url = resultTitle.attr('href');
             
+            // gets the date of the posted job
             const resultDate= $(element).children("time");
             const date = new Date(resultDate.attr('datetime'));
 
+            // gets the near by hood of the place 
             const resultHood = $(element).find(".result-hood");
             const hood = resultHood.text();
 
+            // aligns the details into the object notation
             const scrapeResult = {title, url, date, hood};
+            
+            // pushes the object to the array
             scrapeResults.push(scrapeResult);
 
         });
@@ -54,21 +59,33 @@ async function scrapeJobHeader() {
     }   
 }
 
+// function for scraping description of jobs
 async function scrapeDescriptions(jobsWithHeaders) {
+    // handles Promises of array of ojects
     return Promise.all(
+        // handles each object of the array of job
         jobsWithHeaders.map( async job => {
             try {
+
+                // goto the url of the job
                 const htmlResult = await request.get(job.url);
                 const $ = await cheerio.load(htmlResult);
                 
+                // removes the qr code of description
                 $(".print-qrcode-container").remove();
                 
+                //  gets the description and address using selectors
+                // id and tag with class
                 job.description = $("#postingbody").text();
                 job.address = $("div.mapaddress").text();
-    
+                
+                // gets the compensation of money using selector class
+                // then replacing the text with just money
                 const compensationText = $(".attrgroup").children().first().text();
                 job.compensation = compensationText.replace("compensation: ", "");
     
+
+                // returns the job to the same array
                 return job;                
             } catch (error) {
                 console.error(error)
@@ -78,6 +95,7 @@ async function scrapeDescriptions(jobsWithHeaders) {
 }
 
 async function createCSVFile(data) {
+
     let csv = new ObjectsToCsv(data);
 
     //save to file
@@ -87,9 +105,13 @@ async function createCSVFile(data) {
 }
 
 async function scrapeCraigslist() {
+    // calls the header scrapper
     const jobsWithHeaders = await scrapeJobHeader();
-    const jobsFullData = await scrapeDescriptions(jobsWithHeaders);
 
+    // calls the remaining data scrapper
+    const jobsFullData = await scrapeDescriptions(jobsWithHeaders);
+    
+    console.log(jobsFullData)
     await createCSVFile(jobsFullData);
 }
 scrapeCraigslist();
